3e7a4eccbbda55313d4538221c19b6ddc00612a1
VincentTheBrainfuqqer <letsuseaiuntil5144@gmail.com>
2026-05-20 15:12:28 -0400

    complete lab07

---
+++ _builds/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/link.d"
+  "CMakeFiles/solver.dir/examples/example1.cpp.o"
+  "CMakeFiles/solver.dir/examples/example1.cpp.o.d"
+  "solver"
+  "solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kaleka/VincentTheBrainfuqqer/workspace/projects/lab07
+# Build directory: /home/kaleka/VincentTheBrainfuqqer/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/kaleka/VincentTheBrainfuqqer/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/kaleka/VincentTheBrainfuqqer/workspace/projects/lab07/CMakeLists.txt;52;add_test;/home/kaleka/VincentTheBrainfuqqer/workspace/projects/lab07/CMakeLists.txt;0;")
